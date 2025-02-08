7c0519e82a22dc0972df3c4adbd20b79cca83e02
NotACat1 <notacat.mail@gmail.com>
2025-02-08 13:46:46 +0300

    feat: Creating a `CounterPage' page

---
+++ src/pages/CounterPage/CounterPage.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@store/index';
+import { increment, decrement, setValue } from '@store/slices/counterSlice';
+import styles from './CounterPage.module.scss';
+
+const CounterPage = () => {
+  const dispatch = useDispatch();
+  const value = useSelector((state: RootState) => state.counter.value);
+
+  useEffect(() => {
+    document.title = 'Counter Page';
+  }, []);
+
+  return (
+    <div className={styles.counter}>
+      <h2>Counter</h2>
+      <p>Value: {value}</p>
+      <button className="btn btn-primary me-2" onClick={() => dispatch(increment())}>
+        +
+      </button>
+      <button className="btn btn-danger me-2" onClick={() => dispatch(decrement())}>
+        -
+      </button>
+      <button className="btn btn-warning" onClick={() => dispatch(setValue(0))}>
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default CounterPage;
