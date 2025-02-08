750f3df4ce80749ace532035d4f86d3b87019631
NotACat1 <notacat.mail@gmail.com>
2025-02-08 13:55:23 +0300

    feat: Creating a `router`

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '@store/index';
+import Layout from '@components/Layout/Layout';
+import Home from '@pages/Home';
+import CounterPage from '@pages/CounterPage/CounterPage';
+import UserPage from '@pages/UserPage/UserPage';
+import NotFound from '@pages/NotFound/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'counter', element: <CounterPage /> },
+      { path: 'user', element: <UserPage /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
+
+const App: React.FC = () => (
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
+
+createRoot(document.getElementById('root')!).render(<App />);
+
+export default App;
