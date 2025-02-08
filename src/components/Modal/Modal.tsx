c1c1e843250b301cc0857ef1a2e16981d2643f67
NotACat1 <notacat.mail@gmail.com>
2025-02-08 13:52:18 +0300

    feat: Creating the `Modal` component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from 'react';
+import ReactModal from 'react-modal';
+import styles from './Modal.module.scss';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  children: React.ReactNode;
+}
+
+const Modal = ({ isOpen, onClose, title, children }: ModalProps) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+    } else {
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className={styles.modal}
+      overlayClassName={styles.overlay}
+      ariaHideApp={false}
+    >
+      <div className={styles.сontent}>
+        {title && <h2 className={styles.title}>{title}</h2>}
+        <button className={styles.button} onClick={onClose}>
+          &times;
+        </button>
+        <div className={styles.body}>{children}</div>
+      </div>
+    </ReactModal>
+  );
+};
+
+export default Modal;
